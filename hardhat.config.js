a6017789f01ed859712a183dfced7bb37a1c8735
Arjun Dev <arjun@Arjuns-MacBook-Pro.local>
2022-07-02 20:57:06 +0530

    feat: deployed to mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -29,7 +29,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     }
   },
   solidity: {
-    version: "0.8.3",
+    version: "0.8.4",
     settings: {
       optimizer: {
         enabled: true,
