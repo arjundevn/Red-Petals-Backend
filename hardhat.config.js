367fe9ea7a361fb230f727af94545d58e77cf05b
Arjun Dev <arjun@Arjuns-MacBook-Pro.local>
2022-07-02 20:53:08 +0530

    feat: configured hardhat.js/ added mumbai support

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config({path:__dirname+'/.env'})
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,6 +17,33 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
-};
+ module.exports = {
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {
+    },
+    mumbai: {
+      url: `https://matic-mumbai.chainstacklabs.com`,
+      chainId: 80001,
+      accounts: [`${process.env.DEPLOYER_PRIVATE_KEY}`]
+    }
+  },
+  solidity: {
+    version: "0.8.3",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
